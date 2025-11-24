dc67dd10655ceba716127e2e32ab4cc35b3fdc36
AlexisMtks <62614855+AlexisMtks@users.noreply.github.com>
2025-11-24 09:06:58 +0100

    Create stripe-server.ts

---
+++ src/lib/stripe-server.ts
@@ -0,0 +1,10 @@
+// src/lib/stripe-server.ts
+import Stripe from "stripe";
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not set");
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2024-06-20", // ou la dernière version dispo dans ton dashboard
+});
